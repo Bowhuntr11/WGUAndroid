3e86630807d7f3541d80573d885b2c88c72444fe
Blake Sherrill <bowhuntr11@hotmail.com>
2018-01-22 07:41:13 -0600

    Testing

---
+++ app/src/main/java/com/example/wguandroid/DBOpenHelper.java
@@ -0,0 +1,43 @@
+package com.example.wguandroid;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    //Constants for identifying table and columns
+    public static final String TABLE_TERMS = "terms";
+    public static final String TERM_ID = "_id";
+    public static final String TERM_NAME = "termName";
+    public static final String TERM_CREATED = "termCreated";
+    public static final String[] ALL_COLUMNS =
+            {TERM_ID, TERM_NAME, TERM_CREATED};
+    //Constants for db name and version
+    private static final String DATABASE_NAME = "terms.db";
+    private static final int DATABASE_VERSION = 1;
+    //SQL to create table
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_TERMS + " (" +
+                    TERM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    TERM_NAME + " TEXT, " +
+                    TERM_CREATED + " TEXT default CURRENT_TIMESTAMP" +
+                    ")";
+
+
+    public DBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TERMS);
+        onCreate(db);
+    }
+}
